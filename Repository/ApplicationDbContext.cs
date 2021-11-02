a6476a406a73293782a0d0578e15495b87590c45
rjpikus <rj#pat.ai>
2021-11-02 12:55:36 -0400

    review and update

---
+++ Repository/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.EntityMapper;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+
+namespace Repository
+{
+    public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CustomerMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Services/Interface1.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+using System.Collections.Generic;
+
+namespace Services
+{
+    public interface ICustomerService
+    {
+        IEnumerable<Customer> GetAllCustomers();
+        Customer GetCustomer(int id);
+        void InsertCustomer(Customer customer);
+        void UpdateCustomer(Customer customer);
+        void DeleteCustomer(int id);
+    }
+}
